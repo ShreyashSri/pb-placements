818f2e0bdccb6f965dd3678a54b21f36adb2180a
ShreyashSri <shreyash.srivastava@pointblank.club>
2025-06-22 03:31:20 +0530

    Fixed build fail

--- app/api/resume/upload/route.ts
+++ app/api/resume/upload/route.ts
@@ -7,7 +7,7 @@ import { Buffer } from 'buffer';
 export const dynamic = 'force-dynamic';
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export async function POST(request: NextRequest) {
